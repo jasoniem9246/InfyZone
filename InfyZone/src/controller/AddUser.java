e8fa32ad03b33ff078390af2513176535e1a3917
diyangqiu <wilson19881229@gmail.com>
2015-07-14 09:53:23 -0400

    AddUser Servlet Completed - Diyang

--- InfyZone/src/controller/AddUser.java
+++ InfyZone/src/controller/AddUser.java
@@ -1,11 +1,16 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.DemoUser;
 
 /**
  * Servlet implementation class AddUser
@@ -27,6 +32,7 @@ public class AddUser extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		doPost(request,response);
 	}
 
 	/**
@@ -34,6 +40,44 @@ public class AddUser extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+//		String UserID = request.getParameter("userId");
+//		int UserId = Integer.parseInt(UserID); 
+		String UserName = request.getParameter("userName");
+		String UserPassword = request.getParameter("password");
+		
+//		LocalDateTime date = LocalDateTime.now();
+		EntityManager em = mytools.DBUtil.getEmFactory().createEntityManager();
+		EntityTransaction trans = em.getTransaction();
+		trans.begin();
+		
+		
+		try
+		{
+			
+			
+			DemoUser user = new DemoUser();
+//			user.setUserId(UserId);
+			user.setUserName(UserName);
+			user.setPassword(UserPassword);
+//			user.setCreatedOn(date);
+			em.persist(user);
+			
+			trans.commit();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			trans.rollback();
+		}
+		finally
+		{
+			em.close();
+		}
+		
+		getServletContext()
+		.getRequestDispatcher("/postaddcustomer.jsp")
+		.forward(request, response);
 	}
 
 }
